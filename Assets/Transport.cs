8fee2474f3c519e91ccb11b2b13335956bf80ae1
Adam Streck <adam.streck@gmail.com>
2013-08-04 18:14:59 +0200

    Flying created.

---
+++ Assets/Transport.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transport : MonoBehaviour {
+	Vector3 forward = Vector3.one; 	//< Vector storing last forward direction
+	bool is_ctrl_pressed = false;
+	float move_speed = 2f; //< Speed of moving with "fly mode"
+	
+	void Update() {
+		CharacterMotor motor = GetComponent<CharacterMotor>();
+		CharacterController controller = GetComponent<CharacterController>();
+		
+		// Control is pressed - set up "fly mode"
+		if (Input.GetKeyDown(KeyCode.LeftControl) && controller.isGrounded) {
+			forward = controller.transform.forward;
+			is_ctrl_pressed = true;
+			motor.movement.gravity = 0;
+		}
+		// Control is released - return to the original mode.
+		if (Input.GetKeyUp(KeyCode.LeftControl)) {
+			is_ctrl_pressed = false;
+			motor.movement.gravity = -Physics.gravity.y;
+		}
+		// Control is held - continue "flying".
+		if (Input.GetKey(KeyCode.LeftControl)) {
+			controller.Move(forward * Time.deltaTime * move_speed);
+		}		
+	}		
+}
