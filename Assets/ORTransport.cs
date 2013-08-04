4a495dc6feac8bcfea64781e00c122abfc4578d0
Adam Streck <adam.streck@gmail.com>
2013-08-04 18:55:28 +0200

    Fixed the direction.

--- Assets/ORTransport.cs
+++ Assets/ORTransport.cs
@@ -1,22 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Transport : MonoBehaviour {
+public class ORTransport : MonoBehaviour {
 	Vector3 forward = Vector3.one; 	//< Vector storing last forward direction
 	float move_speed = 2f; //< Speed of moving with "fly mode"
 	
 	void Update() {
-		CharacterMotor motor = GetComponent<CharacterMotor>();
+		// CharacterMotor motor = GetComponent<CharacterMotor>();
 		CharacterController controller = GetComponent<CharacterController>();
+		OVRPlayerController cam = GetComponent<OVRPlayerController>();
 		
 		// Control is pressed - set up "fly mode"
 		if (Input.GetKeyDown(KeyCode.LeftControl) && controller.isGrounded) {
-			forward = controller.transform.forward;
-			motor.movement.gravity = 0;
+			forward = GameObject.Find("ForwardDirection").transform.forward;
+			// motor.movement.gravity = 0;
 		}
 		// Control is released - return to the original movement.
 		if (Input.GetKeyUp(KeyCode.LeftControl)) {
-			motor.movement.gravity = -Physics.gravity.y;
+
+			// motor.movement.gravity = -Physics.gravity.y;
 		}
 		// Control is held - continue movement.
 		if (Input.GetKey(KeyCode.LeftControl)) {
