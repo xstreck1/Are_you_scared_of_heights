32d81f4d3135323902228f5fa51fc2b12cebe6bf
Adam Streck <adam.streck@gmail.com>
2013-08-05 10:18:24 +0200

    OVR movement script is now also working.

--- Assets/ORTransport.cs
+++ Assets/ORTransport.cs
@@ -4,21 +4,25 @@ using System.Collections;
 public class ORTransport : MonoBehaviour {
 	Vector3 forward = Vector3.one; 	//< Vector storing last forward direction
 	float move_speed = 2f; //< Speed of moving with "fly mode"
+	float grav_modif = 0f; //< Storing original gravity modifier
+	OVRPlayerController player; //< Reference to the player object
+	CharacterController controller; //< Reference to the controller object.
+	
+	void Awake() {
+		player = GetComponent<OVRPlayerController>();
+		controller = GetComponent<CharacterController>();
+		grav_modif = player.GravityModifier;
+	}
 	
 	void Update() {
-		// CharacterMotor motor = GetComponent<CharacterMotor>();
-		CharacterController controller = GetComponent<CharacterController>();
-		OVRPlayerController cam = GetComponent<OVRPlayerController>();
-		
 		// Control is pressed - set up "fly mode"
 		if (Input.GetKeyDown(KeyCode.LeftControl) && controller.isGrounded) {
 			forward = GameObject.Find("ForwardDirection").transform.forward;
-			// motor.movement.gravity = 0;
+			player.GravityModifier = 0f;
 		}
 		// Control is released - return to the original movement.
 		if (Input.GetKeyUp(KeyCode.LeftControl)) {
-
-			// motor.movement.gravity = -Physics.gravity.y;
+			player.GravityModifier = grav_modif;
 		}
 		// Control is held - continue movement.
 		if (Input.GetKey(KeyCode.LeftControl)) {
