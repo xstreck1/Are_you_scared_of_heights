cd6c8f86adcc1f67170ddf7071daee2fe45e2b89
Adam Streck <adam.streck@gmail.com>
2013-08-07 12:05:57 +0200

    Basic respawn added.

---
+++ Assets/Respawn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour {
+	Vector3 respaw_pos = Vector3.zero;
+	OVRPlayerController player; //< Reference to the player object
+	const float DEATH_HEIGHT = -30f;
+	const string RESPAWN_NAME = "Respawn";
+	
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<OVRPlayerController>();
+		respaw_pos = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.transform.position.y < DEATH_HEIGHT) {
+			player.Stop();
+			player.transform.position = respaw_pos;
+		}
+	}
+	
+	// 
+	void OnTriggerEnter(Collider other) {
+		if (other.name.Contains(RESPAWN_NAME)) 
+        	respaw_pos = other.transform.position;
+    }
+}
