0863d01d987dfa0807ace25d6b4f5e15f1fc679f
Adam Streck <adam.streck@gmail.com>
2013-08-08 12:20:36 +0200

    Platform and it's sound has been added.

--- Assets/ORTransport.cs
+++ Assets/ORTransport.cs
@@ -1,29 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Class that allows the player to fly when space is pressed.
 public class ORTransport : MonoBehaviour {
 	Vector3 forward = Vector3.one; 	//< Vector storing last forward direction
 	float move_speed = 2f; //< Speed of moving with "fly mode"
 	float grav_modif = 0f; //< Storing original gravity modifier
 	OVRPlayerController player; //< Reference to the player object
 	CharacterController controller; //< Reference to the controller object.
+	GameObject forward_vector;
+	GameObject platform;
 	
 	void Awake() {
 		player = GetComponent<OVRPlayerController>();
 		controller = GetComponent<CharacterController>();
 		grav_modif = player.GravityModifier;
+		forward_vector = GameObject.Find("ForwardDirection");
+		platform = GameObject.Find("Platform");
 	}
 	
 	void Update() {
 		// Control is pressed - set up "fly mode"
 		if (Input.GetKeyDown(KeyCode.LeftControl) && controller.isGrounded) {
-			forward = GameObject.Find("ForwardDirection").transform.forward;
+			forward = forward_vector.transform.forward;
 			player.Stop();
 			player.GravityModifier = 0f;
+			platform.renderer.enabled = true;
+			platform.audio.enabled = true;
 		}
 		// Control is released - return to the original movement.
 		if (Input.GetKeyUp(KeyCode.LeftControl)) {
 			player.GravityModifier = grav_modif;
+			platform.renderer.enabled = false;
+			platform.audio.enabled = false;
 		}
 		// Control is held - continue movement.
 		if (Input.GetKey(KeyCode.LeftControl)) {
--- Assets/Respawn.cs
+++ Assets/Respawn.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Class that covers respawning.
 public class Respawn : MonoBehaviour {
-	Vector3 respaw_pos = Vector3.zero;
+	Vector3 respaw_pos = Vector3.zero; //< Position where the player currently respawns.
 	OVRPlayerController player; //< Reference to the player object
-	const float DEATH_HEIGHT = -30f;
-	const string RESPAWN_NAME = "Respawn";
+	const float DEATH_HEIGHT = -30f; //< Where to start dying.
+	const string RESPAWN_NAME = "Respawn"; //< Names of the obects that will cause respawn.
 	
 	// Use this for initialization
 	void Start () {
@@ -21,7 +22,7 @@ public class Respawn : MonoBehaviour {
 		}
 	}
 	
-	// 
+	// What to do when trigger is hit
 	void OnTriggerEnter(Collider other) {
 		if (other.name.Contains(RESPAWN_NAME)) 
         	respaw_pos = other.transform.position;
