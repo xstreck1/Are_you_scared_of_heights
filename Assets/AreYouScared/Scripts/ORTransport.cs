aca4bc5c26ca28d1cd0299aaa242ad778b925b75
Adam Streck <adam.streck@gmail.com>
2013-08-09 12:34:00 +0200

    Now clean platform movement.

--- Assets/AreYouScared/Scripts/ORTransport.cs
+++ Assets/AreYouScared/Scripts/ORTransport.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 // Class that allows the player to fly when space is pressed.
 public class ORTransport : MonoBehaviour {
-	Vector3 forward = Vector3.one; 	//< Vector storing last forward direction
-	float move_speed = 2f; //< Speed of moving with "fly mode"
-	float grav_modif = 0f; //< Storing original gravity modifier
+	const UnityEngine.KeyCode FLY_KEY = KeyCode.Space;
+	
 	OVRPlayerController player; //< Reference to the player object
 	CharacterController controller; //< Reference to the controller object.
 	GameObject forward_vector; //< Object storing the forward direction of the player.
 	GameObject platform; //< Platform underneath the player.
+	
+	Vector3 forward = Vector3.one; 	//< Vector storing last forward direction
+	float move_speed = 2f; //< Speed of moving with "fly mode"
+	float grav_modif = 0f; //< Storing original gravity modifier
+	bool flying = false; //< Determines whether the player is flying now.
 	
 	void Awake() {
 		// Locate references
@@ -23,21 +27,23 @@ public class ORTransport : MonoBehaviour {
 	
 	void Update() {
 		// Control is pressed - set up "fly mode"
-		if (Input.GetKeyDown(KeyCode.LeftControl) && controller.isGrounded) {
+		if (Input.GetKeyDown(FLY_KEY) && controller.isGrounded) {
 			forward = forward_vector.transform.forward;
 			player.Stop();
 			player.GravityModifier = 0f;
 			platform.renderer.enabled = true;
 			platform.audio.enabled = true;
+			flying = true;
 		}
 		// Control is released - return to the original movement.
-		if (Input.GetKeyUp(KeyCode.LeftControl)) {
+		if (Input.GetKeyUp(FLY_KEY)) {
 			player.GravityModifier = grav_modif;
 			platform.renderer.enabled = false;
 			platform.audio.enabled = false;
+			flying = false;
 		}
 		// Control is held - continue movement.
-		if (Input.GetKey(KeyCode.LeftControl)) {
+		if (Input.GetKey(FLY_KEY) && flying) {
 			controller.Move(forward * Time.deltaTime * move_speed);
 		}		
 	}		
--- Assets/AreYouScared/Scripts/Respawn.cs
+++ Assets/AreYouScared/Scripts/Respawn.cs
@@ -2,85 +2,102 @@
 using System.Collections;
 
 // Class that covers respawning.
-public class Respawn : MonoBehaviour {
+public class Respawn : MonoBehaviour
+{
 	Vector3 respaw_pos = Vector3.zero; //< Position where the player currently respawns.
 	OVRPlayerController player; //< Reference to the player object
 	const float DEATH_HEIGHT = -30f; //< Where to start dying.
 	const string RESPAWN_NAME = "Respawn"; //< Names of the obects that will cause respawn.
 	
-	Texture2D  black_texture;
-	bool do_fading = false;
-	float fading_counter = 0;
-	const float FADING_TIME = 2;
-	bool do_pause = false;
-	float pause_counter = 0;
-	const float PAUSE_TIME = 2;
-	bool do_lighting = false;
-	float lighting_counter = 0;
-	const float LIGHTING_TIME = 2;
+	// Controls connected to screen fading.
+	Texture2D  black_texture; //< Texture to hold the full black color.
+	enum FadePart // Different parts of the fade event
+	{
+		fall,
+		pause,
+		bright,
+		none
+	};
+	FadePart fade_part = FadePart.none; //< Currently active part of the fading.
+	float fading_counter = 0f;
+	const float FADING_TIME = 2f;
+	float pause_counter = 0f;
+	const float PAUSE_TIME = 2f;
+	float lighting_counter = 0f;
+	const float LIGHTING_TIME = 2f;
 
 	// Use this for initialization
-	void Start () {
-		player = GetComponent<OVRPlayerController>();
-		black_texture = new Texture2D(1,1);
-		black_texture.SetPixel(0,0,new Color(0,0,0,255));
-		black_texture.Apply();
+	void Start ()
+	{
+		player = GetComponent<OVRPlayerController> ();
+		black_texture = new Texture2D (1, 1);
+		black_texture.SetPixel (0, 0, new Color (0, 0, 0, 255));
+		black_texture.Apply ();
 		respaw_pos = player.transform.position;
 	}
-	
+
 	// Update is called once per frame
-	void OnGUI () {
-		if (do_fading == true && fading_counter < FADING_TIME) {
-			fading_counter += Time.deltaTime;
-			float alphaFadeValue = Mathf.Clamp01(fading_counter / FADING_TIME);
+	void Update()
+	{
+		if (player.transform.position.y < DEATH_HEIGHT && fade_part == FadePart.none) {
+			setFadePart(FadePart.fall);
+			Debug.Log("Fall detected.");
+		}
+	}
+	
+	// Set up a new part of the fade event
+	void setFadePart (FadePart _new_fade)
+	{
+		fade_part = _new_fade;
+		fading_counter = pause_counter = lighting_counter = 0f;
+	}
+	
+	// When to draw GUI
+	void OnGUI ()
+	{
+		switch (fade_part) {
+			
+		case FadePart.fall:
+			if (fading_counter < FADING_TIME) {
+				fading_counter += Time.deltaTime;
+				float alphaFadeValue = Mathf.Clamp01 (fading_counter / FADING_TIME);
  
-			GUI.color = new Color(0, 0, 0, alphaFadeValue);
-			GUI.DrawTexture( new Rect(0, 0, Screen.width, Screen.height ), black_texture);
-		}
+				GUI.color = new Color (0, 0, 0, alphaFadeValue);			
+			} else 
+				setFadePart(FadePart.pause); 
+			break;
 		
-		else if (do_fading == true && fading_counter >= FADING_TIME) {
-			do_fading = false;
-			do_pause = true;
-			fading_counter = 0;
-		} 
-		
-		if (do_pause == true && pause_counter < PAUSE_TIME) {
-			if (pause_counter < PAUSE_TIME / 2 && pause_counter + Time.deltaTime >= PAUSE_TIME / 2) {
-				player.Stop();
-				player.transform.position = respaw_pos;					
-			}
-			pause_counter += Time.deltaTime;
+		case FadePart.pause: 
+			if (pause_counter < PAUSE_TIME) {
+				if (pause_counter < PAUSE_TIME / 2 && pause_counter + Time.deltaTime >= PAUSE_TIME / 2) {
+					player.Stop ();
+					player.transform.position = respaw_pos;					
+				}
+				pause_counter += Time.deltaTime;
  
-			GUI.DrawTexture( new Rect(0, 0, Screen.width, Screen.height ), black_texture);
-		}
-		
-		else if (do_pause == true && pause_counter >= PAUSE_TIME) {
-			do_pause = false;
-			do_lighting = true;
-			pause_counter = 0;
-		}
-		
-		else if (do_lighting && lighting_counter < LIGHTING_TIME) {
-			lighting_counter += Time.deltaTime;
-			float alphaFadeValue = Mathf.Clamp01(lighting_counter / LIGHTING_TIME);
+			} else 
+				setFadePart(FadePart.bright);
+			break;
+			
+		case FadePart.bright: 
+			if (lighting_counter < LIGHTING_TIME) {
+				lighting_counter += Time.deltaTime;
+				float alphaFadeValue = Mathf.Clamp01 (lighting_counter / LIGHTING_TIME);
  
-			GUI.color = new Color(0, 0, 0, 1 - alphaFadeValue);
-			GUI.DrawTexture( new Rect(0, 0, Screen.width, Screen.height ), black_texture);			
+				GUI.color = new Color (0, 0, 0, 1 - alphaFadeValue);		
+			} else 
+				setFadePart(FadePart.none);		
+			break;
 		}
 		
-		else if (do_lighting && lighting_counter >= LIGHTING_TIME) {
-			do_lighting = false;
-			lighting_counter = 0;			
-		}
-		
-		else if (player.transform.position.y < DEATH_HEIGHT && !do_fading) {
-			do_fading = true;
-		}
+		if (fade_part != FadePart.none)
+			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), black_texture);			
 	}
 	
 	// What to do when trigger is hit
-	void OnTriggerEnter(Collider other) {
-		if (other.name.Contains(RESPAWN_NAME)) 
-        	respaw_pos = other.transform.position;
-    }
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.name.Contains (RESPAWN_NAME)) 
+			respaw_pos = other.transform.position;
+	}
 }
