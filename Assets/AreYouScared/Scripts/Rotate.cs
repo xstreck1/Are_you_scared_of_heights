09d4796a9f9ee4a5d869647793900a99783ba07a
Adam Streck <adam.streck@gmail.com>
2013-08-11 23:05:29 +0200

    Both controllers work in their bounds.

---
+++ Assets/AreYouScared/Scripts/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	GameObject platform;
+	
+	
+	// Use this for initialization
+	void Start () {
+		platform = GameObject.Find("Rotating");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		platform.transform.Rotate(0f,0.1f,0f);
+	}
+}
